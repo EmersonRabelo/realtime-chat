2e0b7377f82b02dead94ee037f423b084bcdd1c7
EmersonRabelo <emersonrabelo1998@gmail.com>
2023-06-09 02:24:41 -0300

    Fix: Translate some labels

--- client/src/AuthPage.jsx
+++ client/src/AuthPage.jsx
@@ -15,15 +15,15 @@ const AuthPage = (props) => {
     return (
     <div className="background">
         <form onSubmit={onSubmit} className="form-card">
-            <div className="form-title">Welcome 👋</div>
+            <div className="form-title">Seja bem-vindo!</div>
 
-            <div className="form-subtitle">Set a username to get started</div>
+            <div className="form-subtitle">Insira um usuário para iniciar!</div>
 
             <div className="auth">
                 <div className="auth-label">Username</div>
                 <input className="auth-input" name="username" />
                 <button className="auth-button" type="submit">
-                Enter
+                Entrar
                 </button>
             </div>
         </form>
